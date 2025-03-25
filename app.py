bd7da1655c2e4311810f01a2a3a1497f561d4493
k40418111 <kide1748@gmail.com>
2025-03-25 15:14:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def handle_message(event):
     text1=event.message.text
     response = openai.ChatCompletion.create(
         messages=[
-            {"role": "system", "content":"你現在回答問題時會隨時講冷笑話，最後都會加上 這個笑話讓人冷峻不禁"}
+            {"role": "system", "content":"你現在回答問題時會隨時講冷笑話，最後都會加上 這個笑話讓人冷峻不禁"},
             {"role": "user", "content": text1}
         ],
         model="gpt-4o-mini-2024-07-18",
